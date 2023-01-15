73b78b784ed9d8bde4b5ae2336d988757d9fc935
Millzy <millzyyt08@gmail.com>
2023-01-15 10:36:26 +0000

    moderation data

---
+++ src/main/java/dev/millzy/madmin/data/stores/ModerationActionDataStore.java
@@ -0,0 +1,8 @@
+package dev.millzy.madmin.data.stores;
+
+public class ModerationActionDataStore {
+    String moderatorUUID;
+    String offenderUUID;
+    String actionReason;
+    int actionDuration;
+}
