aee4ac8249030d2f6826c4f4272c78cec488ace9
Millzy <millzyyt08@gmail.com>
2023-01-16 16:19:14 +0000

    rewriting again

--- src/main/java/dev/millzy/madmin/data/Data.java
+++
@@ -1,5 +0,0 @@
-package dev.millzy.madmin.data;
-
-public abstract class Data {
-    public Data() {}
-}
--- src/main/java/dev/millzy/madmin/data/stores/ModerationActionDataStore.java
+++
@@ -1,8 +0,0 @@
-package dev.millzy.madmin.data.stores;
-
-public class ModerationActionDataStore {
-    String moderatorUUID;
-    String offenderUUID;
-    String actionReason;
-    int actionDuration;
-}
